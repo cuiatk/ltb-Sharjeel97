e2d0533d12eef43baf7704a9d24287fdff9e8c9f
Sharjeel <Sharjeelafzal97@gmail.com>
2019-06-13 11:40:59 +0500

    Full Completed Task

--- ltb-Sharjeel97-master/src/labTerminalB/Customer.java
+++ ltb-Sharjeel97-master/src/labTerminalB/Customer.java
@@ -3,18 +3,21 @@ package labTerminalB;
 import java.util.Enumeration;
 import java.util.Vector;
 
-public class Customer {
-		private String _name;
-		private Vector<Rental> _rentals = new Vector<Rental>();
-		public Customer (String name){
-			_name = name;
-		};
-		public void addRental(Rental arg) {
-			_rentals.addElement(arg);
-		}
-		public String getName (){
-			return _name;
-		};
+class Customer {
+
+private String _name; 
+private Vector _rentals = new Vector();
+
+public Customer (String name){ 
+	_name = name;
+	};
+
+public void addRental(Rental arg) {
+	_rentals.addElement(arg); 
+	}
+public String getName (){
+	return _name;
+}
 		
 		/*
 		 * TODO 4		20 Marks
@@ -31,47 +34,33 @@ public class Customer {
 		 * @param: Nothing
 		 * @return: Result which contain Name of customer,Movie title,Movie price,Total Amount owned and Frequent renter points 
 		 */
-		public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration<Rental> rentals = _rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) rentals.nextElement();
-			//determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
-			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&&
-					each.getDaysRented() > 1) frequentRenterPoints ++;
-			//show figures for this rental
-			result += "\t" + each.getMovie().getTitle()+ "\t" +
-					String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
-		}
-		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) +
-				"\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-		+
-		" frequent renter points";
-		return result;
+public String statement() {
+
+	double totalAmount = 0; 
+	int frequentRenterPoints = 0;
+	Enumeration rentals = _rentals.elements(); 
+	String result = "Rental Record for " + getName() + "\n"; 
+	while (rentals.hasMoreElements()) {
+
+	double thisAmount = 0;
+
+	Rental rental = (Rental) rentals.nextElement();
+
+	//determine amounts for each line 
+	//thisAmount = rental.amount_Calculation();
+
+	// add frequent renter points 
+	frequentRenterPoints += rental.getFrequentPoints(rental);
+
+	//show figures for this rental 
+	result += "\t" + rental.getMovie().getTitle()+ "\t" + String.valueOf(rental.amount_Calculation()) + "\n";
+	totalAmount += rental.amount_Calculation();
+
+	} //add footer lines
+	result += "Amount owned is " + String.valueOf(totalAmount) +"\n";
+	result += "You earned " + String.valueOf(frequentRenterPoints)+ " frequent renter points"; 
+	return result;
+
 	}
-}
+
+};
--- ltb-Sharjeel97-master/src/labTerminalB/Movie.java
+++ ltb-Sharjeel97-master/src/labTerminalB/Movie.java
@@ -1,9 +1,9 @@
 package labTerminalB;
 
 public class Movie {
-	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
+	public static final int CHILDRENS = 2;
 	private String _title;
 	private int _priceCode;
 	public Movie(String title, int priceCode) {
